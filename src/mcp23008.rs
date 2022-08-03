8dbf41d701da16dca642be25413f2f16a39c9603
SingularitySurfer <norman_krackow@gmx.de>
2022-08-03 17:10:41 +0200

    rework mcp23008. comments not done and untested.

--- src/mcp23008.rs
+++ src/mcp23008.rs
@@ -93,113 +93,63 @@ where
         Ok(data[0])
     }
 
-    fn read_double_register(&mut self, reg: Register) -> Result<u16, E> {
-        let mut data = [0u8; 2];
-        self.com.write_read(self.address, &[reg as u8], &mut data)?;
-        Ok(u16::from_le_bytes(data))
-    }
-
     fn write_register(&mut self, reg: Register, data: u8) -> Result<(), E> {
         self.com.write(self.address, &[reg as u8, data])
     }
 
-    fn write_double_register(&mut self, reg: Register, data: u16) -> Result<(), E> {
-        let data = data.to_le_bytes();
-        self.com.write(self.address, &[reg as u8, data[0], data[1]])
-    }
-
     /// Reads a single port, A or B, and returns its current 8 bit value.
-    pub fn read_gpio(&mut self, port: Port) -> Result<u8, E> {
-        self.read_register(port.into())
-    }
-
-    /// Reads all 16 pins (port A and B) into a single 16 bit variable.
-    pub fn read_gpioab(&mut self) -> Result<u16, E> {
-        self.read_double_register(Register::GPIOA)
+    pub fn read_gpio(&mut self) -> Result<u8, E> {
+        self.read_register(Register::GPIO)
     }
 
     /// Writes all the pins of one port with the value at the same time.
-    pub fn write_gpio(&mut self, port: Port, value: u8) -> Result<(), E> {
-        self.write_register(port.into(), value)
+    pub fn write_gpio(&mut self, value: u8) -> Result<(), E> {
+        self.write_register(Register::GPIO, value)
     }
 
-    /// Writes all the pins with the value at the same time.
-    pub fn write_gpioab(&mut self, value: u16) -> Result<(), E> {
-        self.write_double_register(Register::GPIOA, value)
-    }
-
-    /// Sets all pins' modes to either `Mode::Input` or `Mode::Output`.
-    pub fn set_mode_all(&mut self, mode: Mode) -> Result<(), E> {
-        let data = if mode == Mode::Input { 0xffff } else { 0x0000 };
-        self.write_double_register(Register::IODIRA, data)
-    }
-
-    fn map_pin(pin: Pin, rega: Register, regb: Register) -> (Register, usize) {
-        let pin = pin as usize;
-        (if pin < 8 { rega } else { regb }, pin & 7)
-    }
-
-    fn bit(&mut self, pin: Pin, a_reg: Register, b_reg: Register) -> Result<bool, E> {
-        let (reg, pin) = Self::map_pin(pin, a_reg, b_reg);
+    fn bit(&mut self, pin: Pin, reg: Register) -> Result<bool, E> {
         let data = self.read_register(reg)?;
-        Ok(data.get_bit(pin))
+        Ok(data.get_bit(pin as usize))
     }
 
     /// Updates a single bit in the register associated with the given pin.
     /// This will read the register (`port_a_reg` for pins 0-7, `port_b_reg` for the other eight),
     /// set the bit (as specified by the pin position within the register), and write the register
     /// back to the device.
-    fn set_bit(
-        &mut self,
-        pin: Pin,
-        value: bool,
-        a_reg: Register,
-        b_reg: Register,
-    ) -> Result<(), E> {
-        let (reg, pin) = Self::map_pin(pin, a_reg, b_reg);
+    fn set_bit(&mut self, pin: Pin, value: bool, reg: Register) -> Result<(), E> {
         let mut data = self.read_register(reg)?;
-        data.set_bit(pin, value);
+        data.set_bit(pin as usize, value);
         self.write_register(reg, data)
     }
 
     /// Sets the mode for a single pin to either `Mode::Input` or `Mode::Output`.
     pub fn set_mode(&mut self, pin: Pin, mode: Mode) -> Result<(), E> {
-        self.set_bit(pin, mode == Mode::Input, Register::IODIRA, Register::IODIRB)
+        self.set_bit(pin, mode == Mode::Input, Register::IODIR)
     }
 
     /// Writes a single bit to a single pin (GPIOA/GPIOB).
     /// This function accesses the GPIOA/GPIOB registers.
     pub fn write_pin(&mut self, pin: Pin, value: Level) -> Result<(), E> {
-        self.set_bit(pin, value == Level::High, Register::GPIOA, Register::GPIOB)
+        self.set_bit(pin, value == Level::High, Register::GPIO)
     }
 
     /// Reads a single pin (GPIOA/GPIOB).
     pub fn read_pin(&mut self, pin: Pin) -> Result<bool, E> {
-        self.bit(pin, Register::GPIOA, Register::GPIOB)
+        self.bit(pin, Register::GPIO)
     }
 
     /// Writes a single bit to a single pin (OLATA/OLATB).
     pub fn write_output_latch(&mut self, pin: Pin, value: Level) -> Result<(), E> {
-        self.set_bit(pin, value == Level::High, Register::OLATA, Register::OLATB)
+        self.set_bit(pin, value == Level::High, Register::OLAT)
     }
 
     /// Enables or disables the internal pull-up resistor for a single pin (GPPUA/GPPUB).
     pub fn set_pull_up(&mut self, pin: Pin, value: PullUp) -> Result<(), E> {
-        self.set_bit(
-            pin,
-            value == PullUp::Enabled,
-            Register::GPPUA,
-            Register::GPPUB,
-        )
+        self.set_bit(pin, value == PullUp::Enabled, Register::GPPU)
     }
 
     /// Inverts the input polarity for a single pin (IPOLA/IPOLB).
     pub fn set_input_polarity(&mut self, pin: Pin, value: Polarity) -> Result<(), E> {
-        self.set_bit(
-            pin,
-            value == Polarity::Inverted,
-            Register::IPOLA,
-            Register::IPOLB,
-        )
+        self.set_bit(pin, value == Polarity::Inverted, Register::IPOL)
     }
 }
